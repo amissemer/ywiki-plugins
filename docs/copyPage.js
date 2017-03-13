08b204bdbb3207fa0fd54b5dc16de6ac86f589a4
Adrien MISSEMER <adrien.missemer@hybris.com>
2017-03-13 15:52:35 -0400

    confluence module

--- docs/copyPage.js
+++ docs/copyPage.js
@@ -1,73 +1,105 @@
-var spaceKey='ps';
-/**
- * Get a page by spaceKey and title from Confluence and returns a deferred for that page.
- * See jQuery.ajax().done()
- * Failures are logged and ignored.
- * The deferred is resolved with the first matching page is any, else it is rejected.
- */
-var getContent = function(spaceKey,pageTitle,expand) {
-  if (expand) {
-    expand = '&expand='+expand;
-  }
-  var defer = $.Deferred();
-  jQuery.ajax( 
-    {
-      url: contextPath + '/rest/api/content?type=page&spaceKey='+spaceKey+'&limit=1&title=' + pageTitle + expand,
-      success: function (response) {
-        console.log("Filtering AJAX response",response);
-        if (response.results && response.results.length>0) {
-          var page = response.results[0];
-          console.log("Returning ",page);
-          defer.resolve(page);
+var confluence = (function () {
+
+  /**
+   * Get a page by spaceKey and title from Confluence and returns a deferred for that page.
+   * See jQuery.ajax().done()
+   * Failures are logged and ignored.
+   * The deferred is resolved with the first matching page is any, else it is rejected.
+   */
+  var getContent = function(spaceKey,pageTitle,expand) {
+    var expandParam="";
+    if (expand) {
+      expandParam = '&expand='+encodeURIComponent(expand);
+    }
+    var defer = $.Deferred();
+    var url = contextPath + '/rest/api/content?type=page&spaceKey='+encodeURIComponent(spaceKey)+'&limit=1&title=' + encodeURIComponent(pageTitle) + expandParam;
+    console.log(url);
+    jQuery.ajax( 
+      {
+        url: url,
+        success: function (response) {
+          console.log("Filtering AJAX response",response);
+          if (response.results && response.results.length>0) {
+            var page = response.results[0];
+            console.log("Returning ",page);
+            defer.resolve(page);
+          } else {
+            defer.reject("Page Not found: '"+spaceKey+":"+pageTitle+"'");
+          }
+        },
+        error: function (jqr, status, error) {
+          defer.reject(status, error, jqr);
         }
-        defer.reject("Page Not found: '"+spaceKey+":"+pageTitle+"'");
-      },
-      error: ajaxQueryFailureHandler
-    });
-  return defer;
-}
-/** 
- * Copy the page "fromPageTitle" and all its descendants under the page "toPageTitle", 
- * and do a placeholder replacement in each page title using the titleReplacements.
- * Available placeholders are: [Customer] [ProjectName] [Date]
- */
- var copyPage = function(fromPageTitle, toPageTitle, titleReplacements) {
-  getContent(spaceKey, fromPageTitle, 'space,body.storage')
-  .done(function(pageToCopy) {
-      console.log("Found page to Copy",pageToCopy);
-      var newTitle = replacePlaceholders(pageToCopy.title,titleReplacements);
-      console.log("New Title for target page: "+newTitle);
-      pageToCopy.title=newTitle;
-      // Create the new page under toPageTitle
-      createPage(pageToCopy,toPageTitle);
-    }
-  );
-};
-var ajaxQueryFailureHandler = function (jqr, status, error) {
-    console.error("ajax request failed "+status,error,jqr);
-};
-var replacePlaceholders=function(template, replacements) {
-  if (typeof replacements === undefined) return str;
-  var result = template;
-  for (var key in replacements) {
-    if (replacements.hasOwnProperty(key)) {
-      var varStr = '['+key+']';
-      if (result.indexOf(varStr) == -1) {
-        console.warn(varStr + " is not used in template",template);
+      });
+    // add a default handler for failures, to make sure they are logged
+    defer.fail(ajaxFailureLogger);
+    return defer;
+  }
+  /** 
+   * Copy the page "fromPageTitle" and all its descendants under the page "toPageTitle", 
+   * and do a placeholder replacement in each page title using the titleReplacements.
+   * Available placeholders are: [Customer] [ProjectName] [Date]
+   */
+  var copyPage = function(fromSpaceKey, fromPageTitle, toSpaceKey, toPageTitle, titleReplacements) {
+    getContent(fromSpaceKey, fromPageTitle, 'space,body.storage')
+    .done(function(pageToCopy) {
+        console.log("Found page to Copy",pageToCopy);
+        var newTitle = replacePlaceholders(pageToCopy.title,titleReplacements);
+        console.log("New Title for target page: "+newTitle);
+        pageToCopy.title=newTitle;
+        // Create the new page under toPageTitle
+        createPage(pageToCopy,toSpaceKey,toPageTitle);
       }
-      var result = result.split(varStr).join(replacements[key]);
+    );
+  };
+  var ajaxFailureLogger = function (status, error, jqr) {
+      console.error("ajax request failed "+status, error, jqr);
+  };
+  var replacePlaceholders=function(template, replacements) {
+    if (typeof replacements === undefined) return str;
+    var result = template;
+    for (var key in replacements) {
+      if (replacements.hasOwnProperty(key)) {
+        var varStr = '['+key+']';
+        if (result.indexOf(varStr) == -1) {
+          console.warn(varStr + " is not used in template",template);
+        }
+        var result = result.split(varStr).join(replacements[key]);
+      }
     }
+    if (result.indexOf('[')!=-1) {
+      console.warn("title still has uninterpolated variables",result);
+    }
+    return result;
   }
-  if (result.indexOf('[')!=-1) {
-    console.warn("title still has uninterpolated variables",result);
+  var createPage = function(page, targetSpaceKey, targetParentTitle) {
+    return getContent(targetSpaceKey,targetParentTitle,'space')
+    .pipe(function(newParentPage) {
+      console.log("newParentPage=",newParentPage);
+      page.ancestors=[ { id: newParentPage.id } ];
+      console.log("New Page",page);
+      page.space=newParentPage.space
+      return jQuery.ajax( 
+        {
+          url: contextPath + '/rest/api/content',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify(page)
+        }).fail( function(jqr, status, error) {
+          ajaxFailureLogger(status,error,jqr);
+        });
+    });
   }
-  return result;
-}
-var createPage = function(page,parentTitle) {
-  getContent(spaceKey,parentTitle).done(function(newParentPage) {
-    console.log("newParentPage=",newParentPage);
-  })
-}
+
+  // exported functions
+  return {
+    getContent: getContent,
+    copyPage: copyPage,
+    createPage: createPage
+  }
+
+})();
+
           //   if (newParentId) {
           //   response.ancestors[0].id = newParentId;
           // }
@@ -81,12 +113,13 @@ var createPage = function(page,parentTitle) {
           //     }
           // });
 
-copyPage("Capabilities Workshop - [Customer] - [ProjectName] [Date]", "Hybris Capabilities Workshop Dashboard", 
-{
-  "Customer": "Lids",
-  "ProjectName": "Release 2",
-  "Date": "Sept. 2017"
-});
+confluence.copyPage("ps", "Capabilities Workshop - [Customer] - [ProjectName] [Date]", "~adrien.missemer@hybris.com", "Tests", 
+  {
+    "Customer": "Lids",
+    "ProjectName": "Release 2",
+    "Date": "Sept. 2017"
+  }
+);//.done(function( val ) {console.log("Copied",val)}).fail(function(err) {console.error("Error",err)});
 
 
 
