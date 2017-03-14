0a145b53c5b99986dafe8e7925bf2ce75e7f5c51
Adrien MISSEMER <adrien.missemer@hybris.com>
2017-03-13 22:09:31 -0400

    new loader

--- docs/loadscript.js
+++ docs/loadscript.js
@@ -1,4 +1,5 @@
-function loadScript(url, callback){
+var yWikiPlugins = (function () {
+  var loadScript = function (url, callback){
 
     var script = document.createElement("script")
     script.type = "text/javascript";
@@ -8,15 +9,43 @@ function loadScript(url, callback){
             if (script.readyState == "loaded" ||
                     script.readyState == "complete"){
                 script.onreadystatechange = null;
-                callback();
+                if (typeof callback === "function") {callback();}
             }
         };
     } else {  //Others
         script.onload = function(){
-            callback();
+            if (typeof callback === "function") {callback();}
         };
     }
 
     script.src = url;
     document.getElementsByTagName("head")[0].appendChild(script);
-}
+  }
+
+  var bootstrap = function (host, callback) {
+    this.host=host;
+    loadScript (host+'/main.js',callback);
+    loadStyleSheet(host+'/main.css');
+  };
+
+  var getHost = function () {
+    return this.host;
+  }
+
+  var loadStyleSheet = function (url) {
+    var link = document.createElement('link')
+    link.setAttribute('rel', 'stylesheet')
+    link.setAttribute('type', 'text/css')
+    link.setAttribute('href', url)
+    document.getElementsByTagName('head')[0].appendChild(link)
+  };
+
+  var host;
+
+  return {
+    loadStyleSheet:loadStyleSheet,
+    loadScript:loadScript,
+    bootstrap:bootstrap,
+    getHost:getHost
+  }
+})();
