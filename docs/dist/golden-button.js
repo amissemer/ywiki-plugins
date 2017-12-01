1973a16128d434f03163001732054e93d258fdcf
Adrien MISSEMER <adrien.missemer@hybris.com>
2017-11-30 20:48:32 -0500

    attempt to fix ESPLM-540

--- docs/dist/golden-button.js
+++ docs/dist/golden-button.js
@@ -10360,6 +10360,7 @@ function getOriginLocation() {
   var scripts = document.getElementsByTagName('script');
   var l = document.createElement("a");
   l.href = scripts[scripts.length-1].getAttribute("src");
+  if (!l.origin) l.origin=l.protocol+"//"+l.host
   console.log("origin",l.origin);
   return l;
 }
--- js/mainframe/pluginCommon.js
+++ js/mainframe/pluginCommon.js
@@ -2,6 +2,7 @@ function getOriginLocation() {
   var scripts = document.getElementsByTagName('script');
   var l = document.createElement("a");
   l.href = scripts[scripts.length-1].getAttribute("src");
+  if (!l.origin) l.origin=l.protocol+"//"+l.host
   console.log("origin",l.origin);
   return l;
 }
