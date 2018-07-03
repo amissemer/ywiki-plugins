fe0bb8b12b3ba4afe4b62a15bfcca129e914d424
Adrien MISSEMER <adrien.missemer@hybris.com>
2018-07-03 11:37:27 -0400

    fixed the origin detection

--- js/mainframe/pluginCommon.js
+++ js/mainframe/pluginCommon.js
@@ -1,10 +1,32 @@
+const WHITELISTED_ORIGIN = [
+  'https://localhost',
+  'https://amissemer.github.io',
+  'https://es-global-ci.github.io'
+];
+
 function getOriginLocation() {
   var scripts = document.getElementsByTagName('script');
-  var l = document.createElement("a");
-  l.href = scripts[scripts.length-1].getAttribute("src");
-  if (!l.origin) l.origin=l.protocol+"//"+l.host
-  console.log("origin",l.origin);
-  return l;
+  var candidates = [];
+  for (let s of scripts) {
+    var url = s.getAttribute("src");
+    if (url && url.indexOf('http')==0) candidates.push(url);
+  }
+  console.log('Looking for script origin within', candidates);
+
+  for (let idx = candidates.length-1; idx>=0; idx--) {
+    var l = document.createElement("a");
+    l.href = candidates[idx];
+    if (!l.origin) l.origin=l.protocol+"//"+l.host
+    if (whitelistedOrigin(l.origin)) {
+      console.log("Found own origin: ",l.origin, 'cache busting', l.search);
+      return l;
+    }
+  }
+  throw 'Could not find a whitelisted origin';
+}
+
+function whitelistedOrigin(origin) {
+  return WHITELISTED_ORIGIN.indexOf(origin)>=0;
 }
 
 var originlocation = getOriginLocation();
